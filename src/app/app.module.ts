243d1090c68effb3f66a4401fda5322634a9b3ee
Abel-Landeo <abel.landeo@gmail.com>
2017-11-23 12:11:23 -0500

    routing, hotels and cars

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,32 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { AutosComponent } from './autos/autos.component';
+import { HotelesComponent } from './hoteles/hoteles.component';
 
+const appRoutes: Routes = [
+	{ path: 'inicio', component: AppComponent },
+	{ path: 'dashboard', component: DashboardComponent },
+	{ path: 'autos', component: AutosComponent },
+	{ path: 'hoteles', component: HotelesComponent },
+	{ path: '',   redirectTo: '/dashboard', pathMatch: 'full' }
+];
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+	declarations: [
+		AppComponent,
+		DashboardComponent,
+		AutosComponent,
+		HotelesComponent
+	],
+	imports: [
+		BrowserModule,
+		RouterModule.forRoot(appRoutes)
+	],
+	providers: [],
+	bootstrap: [AppComponent]
 })
 export class AppModule { }
